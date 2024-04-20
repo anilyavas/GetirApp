e03ab32389faa8fa5dd123932327b130cb04a749
anilyavas <anil.yavas99@gmail.com>
2024-04-20 13:44:55 +0300

    navigation setup

---
+++ src/navigation/navigation.tsx
@@ -0,0 +1,7 @@
+import { Stack } from 'expo-router';
+
+const Navigation = () => {
+  return <Stack />;
+};
+
+export default navigation;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+const styles = StyleSheet.create({});
