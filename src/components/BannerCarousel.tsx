e0fbaf7c84271df62415a2e4223d7d0c38551e50
anilyavas <anil.yavas99@gmail.com>
2024-04-20 20:36:29 +0300

    banner with flatlist

--- src/components/BannerCarousel.tsx
+++ src/components/BannerCarousel.tsx
@@ -1,12 +1,45 @@
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Image,
+  Dimensions,
+} from 'react-native';
 
+const { width, height } = Dimensions.get('window');
+const banners = [
+  'https://cdn.getir.com/misc/611e55d33ea65bef40f9ba05_banner_tr_1629378026496.jpeg',
+  'https://cdn.getir.com/misc/621784419e62143ed76eef01_banner_tr_1645969386292.jpeg',
+  'https://cdn.getir.com/promos/6221aef965805c5b1e703845_banner_tr_1646723453154.jpeg',
+  'https://cdn.getir.com/misc/622a6d18b2e2fe3a8e809894_banner_tr_1646947639211.jpeg',
+];
 const BannerCarousel = () => {
   return (
-    <View>
-      <Text>BannerCarousel</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={banners}
+        renderItem={(item) => (
+          <Image source={{ uri: item.item }} style={styles.image} />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        snapToInterval={width}
+        snapToAlignment={'center'}
+        decelerationRate={'fast'}
+      />
     </View>
   );
 };
 
 export default BannerCarousel;
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    width: width * 1,
+    height: height * 0.3,
+    resizeMode: 'stretch',
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,14 @@
 import { View, StyleSheet } from 'react-native';
 import HeaderBanner from '../components/HeaderBanner';
 import Address from '../components/Address';
+import BannerCarousel from '../components/BannerCarousel';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <HeaderBanner />
       <Address />
+      <BannerCarousel />
     </View>
   );
 };
