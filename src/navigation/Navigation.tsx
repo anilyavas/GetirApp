a42384af9d1a68f28e9e240d4456b59b3b2da080
anilyavas <anil.yavas99@gmail.com>
2024-04-21 21:19:23 +0300

    tabbar inactive active colors

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -5,6 +5,8 @@ import CategoryDetailsScreen from '../screens/CategoryDetailsScreen';
 import SearchScreen from '../screens/SearchScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import GiftsScreen from '../screens/GiftsScreen';
+import { FontAwesome } from '@expo/vector-icons';
+import { colors } from '../constants/colors';
 
 const Stack = createStackNavigator();
 
@@ -25,15 +27,67 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+      }}
+    >
       <Tab.Screen
         name='Home'
         component={Navigation}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name='home'
+              size={30}
+              color={focused ? colors.primary : 'grey'}
+            />
+          ),
+        }}
       />
-      <Tab.Screen name='Search' component={SearchScreen} />
-      <Tab.Screen name='Profile' component={ProfileScreen} />
-      <Tab.Screen name='Gift' component={GiftsScreen} />
+      <Tab.Screen
+        name='Search'
+        component={SearchScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name='search'
+              size={30}
+              color={focused ? colors.primary : 'grey'}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name='user'
+              size={30}
+              color={focused ? colors.primary : 'grey'}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Gift'
+        component={GiftsScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name='gift'
+              size={30}
+              color={focused ? colors.primary : 'grey'}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
