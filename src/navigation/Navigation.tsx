978afaafd229d08e6fc161ff85ade784b8ab202d
anilyavas <anil.yavas99@gmail.com>
2024-04-21 21:12:19 +0300

    nested navigation

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,17 +1,41 @@
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import CategoryDetailsScreen from '../screens/CategoryDetailsScreen';
+import SearchScreen from '../screens/SearchScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import GiftsScreen from '../screens/GiftsScreen';
 
 const Stack = createStackNavigator();
+
 const Navigation = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen
-        name='Home'
+        name='HomeScreen'
         component={HomeScreen}
         options={{ headerShown: false }}
       />
+      <Stack.Screen name='Details' component={CategoryDetailsScreen} />
     </Stack.Navigator>
   );
 };
 
-export default Navigation;
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name='Home'
+        component={Navigation}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen name='Search' component={SearchScreen} />
+      <Tab.Screen name='Profile' component={ProfileScreen} />
+      <Tab.Screen name='Gift' component={GiftsScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigation;
